de55ec064c961ed8d52c29a4d5f90338b294cb75
karthikkompella160 <66721742+karthikkompella160@users.noreply.github.com>
2020-12-30 00:38:25 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,8 @@
+
+
+import 'package:awsapp/login.dart';
 import 'package:flutter/material.dart';
-import 'login.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -13,8 +16,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         
         
-        body: Login()
+        body: Login()    // replaced login() with Alert box and for testing i kept Connection to both snackbar and alertbox
       ),
     );
   }
+}
+// ignore: must_be_immutable
+class AlertBox  extends StatelessWidget {
+   String data;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("AWS"),
+      actions: [
+      RaisedButton(onPressed: (){
+         snackbar(context); // called snackbar here too 
+      },
+      child:Text("Ok"))
+      ],
+      shape:RoundedRectangleBorder() ,
+      titlePadding: EdgeInsets.symmetric(),
+    );
+  }
+}
+
+
+Future<void> snackbar(BuildContext context)
+async {
+
+SnackBar snackBar=SnackBar(content: Text("i am called"),
+action: SnackBarAction(onPressed: (){},label: "click!",),);
+Scaffold.of(context).showSnackBar(snackBar);
 }
